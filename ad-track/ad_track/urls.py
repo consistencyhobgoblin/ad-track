b173e375c161f214bb8114f4f79476f00a2cbec0
Matt Owen <consistencyhobgoblin-github@yahoo.com>
2020-01-07 13:48:54 -0800

    Add project files.

---
+++ ad-track/ad_track/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for ad_track.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', ad_track.views.home, name='home'),
+    # url(r'^ad_track/', include('ad_track.ad_track.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
